5e414067a6b4aed6b323f7c7fabbeb9636cc3cf5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-07-28 16:41:55 +0100

    Fourth Commit: Set the width and height of the screen

--- main.py
+++ main.py
@@ -15,3 +15,8 @@ import pygame
 
 # Initialize Pygame
 pygame.init()
+
+# Set the width and height of the screen
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
