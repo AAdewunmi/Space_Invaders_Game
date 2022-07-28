27c3c3742994386e5ab29dca1ea919bc14d76d13
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-07-28 15:58:32 +0100

    Initial Commit: Project Set Up and Skeleton main class

---
+++ main.py
@@ -0,0 +1,12 @@
+# Space Invaders Game
+# By: Adrian Adewunmi
+# Date: 28/07/2022
+# Version: 1.0
+# Description: This is the main file for the Space Invaders game.
+#             It contains the main game loop and the main game functions.
+#             It also contains the main menu and the game over screen.
+#             It also contains the high score screen.
+#             It also contains the pause screen.
+#             It also contains the game over screen.
+# Adapted from: https://www.youtube.com/watch?v=FfWpgLFMI7w
+#               https://github.com/attreyabhatt/Space-Invaders-Pygame
