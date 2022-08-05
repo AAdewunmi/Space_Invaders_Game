78041ccbe7ef5eabe65de258fb592675a6b2bf40
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2022-08-05 11:53:47 +0100

    Thirteenth Commit: Implement Keyboard Input (If the keystroke is pressed down, the player will move left or right).

--- main.py
+++ main.py
@@ -43,11 +43,17 @@ app_running = True
 while app_running:
     # Change the background color
     screen.fill((0, 0, 0))
-    # Move the playerX to the right
-    playerX += 0.5
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             app_running = False
+        # Keyboard Input
+        # If the keystroke is pressed, the player will move left or right
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                print('Left Arrow Pressed')
+            if event.key == pygame.K_RIGHT:
+                print('Right Arrow Pressed')
     # Draw the player on the screen
     player(playerX, playerY)
     pygame.display.update()
